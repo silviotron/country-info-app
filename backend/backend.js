09b99c4bcf14d665a06010256e8e79da0aa23c4f
Silvio <silvionovasmartinez@gmail.com>
2023-11-30 18:12:20 +0100

    handled cors + gitignore

--- backend/backend.js
+++ backend/backend.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 
 const app = express()
 const PORT = 5000
 
-
+app.use(cors());
 app.use(express.json())
 
 app.get("/name", async (req, res) => {
