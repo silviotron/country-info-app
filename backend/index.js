8deb20e3a6ed1ca188b7e4be7296193b20fffd8d
Silvio <silvionovasmartinez@gmail.com>
2023-12-01 11:58:09 +0100

    "/info" now uses cca3 to work

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
-const PORT = 5000
+const app = express();
+const PORT = 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.get("/name", async (req, res) => {
-    const { name } = req.query
-    var endpoint = (name === undefined || name.trim() === "") ? `https://restcountries.com/v3.1/all?fields=name,` : `https://restcountries.com/v3.1/name/${name.trim()}?fields=name`
+    const { name } = req.query;
+    var endpoint =
+        name === undefined || name.trim() === ""
+            ? `https://restcountries.com/v3.1/all?fields=name,cca3`
+            : `https://restcountries.com/v3.1/name/${name.trim()}?fields=name,cca3`;
 
     fetch(endpoint)
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
                 if (response.status === 404) {
                     return [];
@@ -22,36 +25,43 @@ app.get("/name", async (req, res) => {
             }
             return response.json();
         })
-        .then(response => {
-            res.json({ "names": response.map(element => element.name.common) });
+        .then((response) => {
+            res.json({
+                names: response.map((element) => ({
+                    [element.cca3]: element.name.common,
+                })),
+            });
         })
-        .catch(error => {
-            console.error('Error:', error.message);
-            res.status(500).json({ error: 'Internal Server Error' });
+        .catch((error) => {
+            console.error("Error:", error.message);
+            res.status(500).json({ error: "Internal Server Error" });
         });
-
-})
+});
 
 app.get("/info", (req, res) => {
-    const { name } = req.query;
-    const endpoint = `https://restcountries.com/v3.1/name/${name.trim()}`;
+    const { cca3 } = req.query;
+    const endpoint = `https://restcountries.com/v3.1/alpha/${cca3}`;
 
     fetch(endpoint)
-        .then(response => {
+        .then((response) => {
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
             if (data.length !== 1) {
-                res.status(400).json({ error: 'Ambiguous request: Multiple countries found with the provided name' });
+                res.status(400).json({
+                    error:
+                        "Ambiguous request: Multiple countries found with the provided code",
+                });
             } else {
                 res.json(data[0]);
             }
         })
-        .catch(error => {
-            console.error('Error:', error.message);
-            res.status(500).json({ error: 'Internal Server Error' });
+        .catch((error) => {
+            console.error("Error:", error.message);
+            res.status(500).json({ error: "Internal Server Error" });
         });
 });
 
-
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
